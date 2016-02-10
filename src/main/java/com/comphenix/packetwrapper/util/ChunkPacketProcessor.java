5836dc95c3d6675d6462903aecc98c032ff16d6b
Janmm14 <computerjanimaus@yahoo.de>
2016-02-10 13:03:46 +0100

    Move ChunkPacketProcessor into util package

--- src/main/java/com/comphenix/packetwrapper/util/ChunkPacketProcessor.java
+++ src/main/java/com/comphenix/packetwrapper/util/ChunkPacketProcessor.java
@@ -16,7 +16,7 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with PacketWrapper.  If not, see <http://www.gnu.org/licenses/>.
  */
-package com.comphenix.packetwrapper;
+package com.comphenix.packetwrapper.util;
 
 import org.bukkit.Location;
 import org.bukkit.World;
